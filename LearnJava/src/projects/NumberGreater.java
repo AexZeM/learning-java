26440a2541d13a5dbf87aa33c02b9977fb30f5dc
AexZeM <kerimalatas08@gmail.com>
2025-08-15 03:50:04 +0300

    If the numbers are equal, it now gives an informational message.

--- LearnJava/src/projects/NumberGreater.java
+++ LearnJava/src/projects/NumberGreater.java
@@ -2,38 +2,35 @@ package projects;
 
 import java.util.Scanner;
 
-/*
- * a > b > c or a > c > b
- * b > a > c or b > c > a
- * c > a > b or c > b > a
- */
 public class NumberGreater {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter numbers: ");
+        System.out.print("Enter three distinct numbers: ");
         int a = input.nextInt();
         int b = input.nextInt();
         int c = input.nextInt();
 
-        if(a > b && a > c){
-            if(b > c){
+        if (a > b && a > c) {
+            if (b > c) {
                 System.out.println("a > b > c");
-            }
-            else{
+            } else {
                 System.out.println("a > c > b");
             }
-        } else if(b > a && b > c){
-            if(a > c){
+        } else if (b > a && b > c) {
+            if (a > c) {
                 System.out.println("b > a > c");
-            }else{
+            } else {
                 System.out.println("b > c > a");
             }
-        } else if(c > a && c > b){
-            if(a > b){
+        } else if (c > a && c > b) {
+            if (a > b) {
                 System.out.println("c > a > b");
-            }else{
+            } else {
                 System.out.println("c > b > a");
             }
+        } else {
+            System.out.println("The numbers are not distinct. Please enter three different numbers.");
         }
+        input.close();
     }
 }
